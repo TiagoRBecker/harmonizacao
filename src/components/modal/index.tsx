37401c8c44b57bc655eec4265ca68acb975819fb
Tiago Becker <beckertiago09@gmail.com>
2023-11-13 16:01:24 -0300

    correcoes

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { Dialog } from "@headlessui/react";
 import { useState } from "react";
-export default function Modal({ isOpen, setIsOpen ,termsAccepted,setTermsAccepted}: any) {
+export default function Modal({
+  isOpen,
+  setIsOpen,
+  termsAccepted,
+  setTermsAccepted,
+}: any) {
   let [nextTerm, setNextTerms] = useState(false);
   const [accepted, setAccepted] = useState(false);
   const [notAccepted, setNotAccepted] = useState(false);
@@ -14,13 +19,15 @@ export default function Modal({ isOpen, setIsOpen ,termsAccepted,setTermsAccepte
         return (
           <>
             <Dialog.Description className="w-full flex items-center justify-center min-h-[200px] text-justify text-base text-[#072137] px-5 py-2 ">
-              Considerando que os documentos criados pela Contratada pertencem a
-              propriedade intelectual da Contratada podendo ser cedido somente
-              para uso exclusivo da clinica, instituto ou profissional ora
-              contratante, sendo vedado a sua divulgação, venda ou
-              compartilhamento com terceiros (DRM- Digital Rights Management). O
-              comprador está ciente que lhe é vedado (proibido) o
-              compartilhamento ou revenda dos documentos adquiridos:
+              Considerando que os documentos criados pela Contratada são de
+              propriedade intelectual da Contratada e cedido somente para uso
+              exclusivo do adquirente profissional ora contratante em seu
+              restrito benefício privado e seu consultório ou instituto na qual
+              atua localmente, é vedado a sua divulgação, venda ou
+              compartilhamento para terceiros. Desta forma o adquirente desta
+              cessão de uso , está plenamente ciente que lhe é vedado (proibido)
+              o compartilhamento, cessão ou qualquer meio de transferência dos
+              documentos adquiridos desta plataforma para terceiros.
             </Dialog.Description>
 
             <div className="flex flex-col gap-2 w-full py-5">
@@ -58,15 +65,14 @@ export default function Modal({ isOpen, setIsOpen ,termsAccepted,setTermsAccepte
           <>
             <Dialog.Description className="w-full flex items-center justify-center min-h-[200px] text-justify text-base text-[#072137] px-5 py-2 ">
               Considerando que os documentos modelos foram elaborados e
-              sugeridos por Advogados, em atividade meio, com o auxílio técnico
-              de profissionais da área da saúde, sendo obrigação da contratante
-              revisar e alterar qualquer conteúdo que, sob a sua ótica, esteja
-              em desacordo com as melhores práticas na área da saúde, bem como
-              eventual evolução das técnicas e recomendações clinicas
-              pertinentes a área de conhecimento da Contratante. Desta forma, A
-              contratante ao utilizar os documentos modelos não promovendo
-              alteração quanto ao conteúdo de saúde ou jurídico estará validando
-              o conteúdo dos modelos automaticamente.
+              sugeridos por Juristas e profissionais da área da saúde, em
+              atividade meio, é obrigação da contratante revisar e alterar
+              qualquer conteúdo que, sob a sua ótica, esteja em desacordo com as
+              melhores práticas na área da saúde e demais assuntos pertinentes a
+              área de conhecimento da Contratante. A contratante ao utilizar os
+              documentos modelos não promovendo alteração no conteúdo de saúde
+              ou jurídico valida o conteúdo dos modelos automaticamente, como
+              adequados.
             </Dialog.Description>
 
             <div className="flex gap-2 w-full py-5">
@@ -105,11 +111,7 @@ export default function Modal({ isOpen, setIsOpen ,termsAccepted,setTermsAccepte
         return (
           <div className="w-full flex flex-col items-center justify-center min-h-[200px] text-justify text-base text-[#072137] px-5 py-2 ">
             <p>
-              Ao aceitar os termos de uso, você concorda em cumprir e respeitar
-              integralmente as seguintes diretrizes e condições estabelecidas
-              neste documento. Estes termos de uso formam um contrato legal
-              entre você e [Nome da Empresa] e regem o uso de nossos serviços e
-              plataformas online.
+            Ao aceitar os termos de uso, você concorda em cumprir e respeitar integralmente as seguintes diretrizes e condições estabelecidas neste documento. Estes termos de uso formam um contrato legal entre você e Harmonização Prontuários e regem o uso de nossos serviços e plataformas online.
             </p>
             <div className="flex gap-2 justify-center items-center w-full ">
               <button
@@ -155,13 +157,13 @@ export default function Modal({ isOpen, setIsOpen ,termsAccepted,setTermsAccepte
   };
   const handleAcceptedTerms = async () => {
     if (accepted === true && notAccepted === true) {
-      setTermsAccepted(true)
-       const request = await fetch("/api/terms",{
-        method:"POST",
-        body:JSON.stringify({accepted,notAccepted})
-       })
-      const reponse = await request.json()
-    
+      setTermsAccepted(true);
+      const request = await fetch("/api/terms", {
+        method: "POST",
+        body: JSON.stringify({ accepted, notAccepted }),
+      });
+      const reponse = await request.json();
+
       setNextTerms(false);
       setIsOpen(false);
       setStep(1);
@@ -169,48 +171,50 @@ export default function Modal({ isOpen, setIsOpen ,termsAccepted,setTermsAccepte
       return alert("Error");
     }
   };
-const handleCancel = () =>{
+  const handleCancel = () => {
     setNextTerms(false);
     setIsOpen(false);
     setStep(1);
-    setAccepted(false)
-    setNotAccepted(false)
-  }
+    setAccepted(false);
+    setNotAccepted(false);
+  };
   return (
-   
-      <Dialog
-        className="  bg-opacity w-full h-full top-0 bottom-0 left-0 z-50  right-0 py-10 absolute "
-        open={isOpen}
-        onClose={handleCancel}
-      >
-        
-        <Dialog.Panel className="flex items-center justify-center flex-col w-[80%] h-full mt-5 mx-auto bg-white px-5 pt-5 rounded-md  relative">
+    <Dialog
+      className="  bg-opacity w-full h-full top-0 bottom-0 left-0 z-50  right-0 py-10 absolute "
+      open={isOpen}
+      onClose={handleCancel}
+    >
+      <Dialog.Panel className="flex items-center justify-center flex-col w-[80%] h-full mt-5 mx-auto bg-white px-5 pt-5 rounded-md  relative">
         <div className="w-full flex items-center justify-end">
-        <button 
-         onClick={() => setIsOpen(false)}
-        className="w-8 h-8  rounded-full border-2 border-red-600">X</button>
+          <button
+            onClick={() => setIsOpen(false)}
+            className="w-8 h-8  rounded-full border-2 border-red-600"
+          >
+            X
+          </button>
         </div>
-       
-          <Dialog.Title className="text-lg md:text-2xl text-[#072137] font-semibold">Termos e Condições de Uso </Dialog.Title>
-          <div>{getLayout()}</div>
-          <div className="flex gap-2">
-            <button
-              className="border-2 border-gray-400 py-1 px-4 bg-slate-400 rounded-md"
-              onClick={handleMenus}
-              disabled={step === 1}
-            >
-              Voltar
-            </button>
-            <button
-              className="border-2 border-gray-400 py-1 px-4 bg-slate-400 rounded-md"
-              onClick={handlePlus}
-              disabled={step === 3}
-            >
-              Proximo
-            </button>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
-  
+
+        <Dialog.Title className="text-lg md:text-2xl text-[#072137] font-semibold">
+          Termos e Condições de Uso{" "}
+        </Dialog.Title>
+        <div>{getLayout()}</div>
+        <div className="flex gap-2">
+          <button
+            className="border-2 border-gray-400 py-1 px-4 bg-slate-400 rounded-md"
+            onClick={handleMenus}
+            disabled={step === 1}
+          >
+            Voltar
+          </button>
+          <button
+            className="border-2 border-gray-400 py-1 px-4 bg-slate-400 rounded-md"
+            onClick={handlePlus}
+            disabled={step === 3}
+          >
+            Proximo
+          </button>
+        </div>
+      </Dialog.Panel>
+    </Dialog>
   );
 }
